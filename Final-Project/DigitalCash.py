9225e3e92beea1b176ece114efcd96d3a0278bee
Noah Smith <nsmith76@emich.edu>
2019-12-10 23:54:14 -0500

    Worked with Zach

--- Final-Project/DigitalCash.py
+++ Final-Project/DigitalCash.py
@@ -6,3 +6,25 @@ Usage of Program:
 python3 DigitalCash.py 
 '''
 
+#Team Created Classes
+import Customer
+import Bank
+
+
+
+identity = int(input("Please enter your ID: "))
+numOfMoneyOrders = int(input("How many money orders would you like to make today?: "))
+valueofMO = []
+
+#Start of Program
+Alice = Customer.CustomerClass(identity=identity)
+#Bob = Bank.bank()
+
+
+
+for i in range(numOfMoneyOrders):
+    inputVal = int(input("What is the value of the Money Order " + str((i+1)) + "?: "))
+    valueofMO.append(inputVal)
+
+for i in range(numOfMoneyOrders):
+    Alice.createMO(valueOfMoneyOrder=valueofMO[i],IDofMO=i)
